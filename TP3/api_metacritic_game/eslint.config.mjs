eaa309c3ab1a7f04645d8ffdcc5d9ff420574f6d
Ambre Rouillon <114724166+Namioumiou@users.noreply.github.com>
2024-05-01 22:30:23 +0200

    Update eslint.config.mjs

--- TP3/api_metacritic_game/eslint.config.mjs
+++ TP3/api_metacritic_game/eslint.config.mjs
@@ -1,7 +1,4 @@
-import globals from "globals";
-
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}}
 ];
